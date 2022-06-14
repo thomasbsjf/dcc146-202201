4a19f95d571030dd68942f10bea05e1092f70859
Thomas Santos <thomasbsjf@gmail.com>
2022-06-14 15:13:18 -0300

    Mensagem Inicial

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void mensagemInicial()
+{
+    cout << "-------------------------------" << "Instrucoes de uso" << "---------------------------------------------" << endl;
+    cout << "---------------------------------------------------------------------------------------------" << endl;
+    cout << ":c file_name  -> " << "Carrega um arquivo com definicoes de tags " << endl;
+    cout << ":l            -> " << "Lista as definicoes de tag validas" << endl;
+    cout << ":q            -> " << "Sai do programa" << endl;
+    cout << ":s file_name  -> " << "Salvar as tags" << endl;
+    cout << ":a            -> " << "Lista as definicoes formais dos automatos em memoria " << "(Nao implementado)" << endl;
+    cout << ":p            -> " << "Realiza a divisao em tags da entrada informada " << "(Nao implementado)" << endl;
+    cout << ":o file_name  -> " << "Especifica o caminho do arquivo de saida para a divisao em tags " << "(Nao implementado)" << endl;
+    cout << ":d file_name  -> " << "Realiza a divisao em tags da string do arquivo informado " << "(Nao implementado)" << endl;
+}
 
 int main()
 {
