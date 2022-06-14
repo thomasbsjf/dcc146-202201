3338ffc9940c1f7cde9f5362ab1e21fe9382f25a
Igor Westermann <i.westma@gmail.com>
2022-06-14 16:26:31 -0300

    add funcoes tag

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-//Alunos: 
-//Denner Efisio Emanuel Reis - 201735008
-//Thomas Santos - 201776034
+// Alunos:
+// Denner Efisio Emanuel Reis - 201735008
+// Thomas Santos - 201776034
+//  Igor Westermann Lima - 201876021
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +13,30 @@ using namespace std;
 
 void mensagemInicial()
 {
-    cout << "-------------------------------" << "Instrucoes de uso" << "---------------------------------------------" << endl;
+    cout << "-------------------------------"
+         << "Instrucoes de uso"
+         << "---------------------------------------------" << endl;
     cout << "---------------------------------------------------------------------------------------------" << endl;
-    cout << ":c file_name  -> " << "Carrega um arquivo com definicoes de tags " << endl;
-    cout << ":l            -> " << "Lista as definicoes de tag validas" << endl;
-    cout << ":q            -> " << "Sai do programa" << endl;
-    cout << ":s file_name  -> " << "Salvar as tags" << endl;
-    cout << ":a            -> " << "Lista as definicoes formais dos automatos em memoria " << "(Nao implementado)" << endl;
-    cout << ":p            -> " << "Realiza a divisao em tags da entrada informada " << "(Nao implementado)" << endl;
-    cout << ":o file_name  -> " << "Especifica o caminho do arquivo de saida para a divisao em tags " << "(Nao implementado)" << endl;
-    cout << ":d file_name  -> " << "Realiza a divisao em tags da string do arquivo informado " << "(Nao implementado)" << endl;
+    cout << ":c file_name  -> "
+         << "Carrega um arquivo com definicoes de tags " << endl;
+    cout << ":l            -> "
+         << "Lista as definicoes de tag validas" << endl;
+    cout << ":q            -> "
+         << "Sai do programa" << endl;
+    cout << ":s file_name  -> "
+         << "Salvar as tags" << endl;
+    cout << ":a            -> "
+         << "Lista as definicoes formais dos automatos em memoria "
+         << "(Nao implementado)" << endl;
+    cout << ":p            -> "
+         << "Realiza a divisao em tags da entrada informada "
+         << "(Nao implementado)" << endl;
+    cout << ":o file_name  -> "
+         << "Especifica o caminho do arquivo de saida para a divisao em tags "
+         << "(Nao implementado)" << endl;
+    cout << ":d file_name  -> "
+         << "Realiza a divisao em tags da string do arquivo informado "
+         << "(Nao implementado)" << endl;
 }
 
 int main()
