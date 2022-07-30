66f07185f142429454110a6e97dc359e6c710014
Thomas Santos <thomasbsjf@gmail.com>
2022-07-30 17:58:05 -0300

    Automato.h e automato.cpp criados

---
+++ src/automato.cpp
@@ -0,0 +1,38 @@
+// Alunos:
+// Denner Efisio Emanuel Reis - 201735008
+// Thomas Santos - 201776034
+// Igor Westermann Lima - 201876021
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <regex>
+#include "automato.h"
+#include "tag.h"
+
+/**
+ * Construtor para inicialização vazia da classe Automato
+ * 
+ *
+*/
+Automato::Automato()
+{
+    
+    //this-> = nullptr;
+}
+
+/**
+ * Destrutor da class Automato
+ * 
+*/
+Automato::~Automato()
+{
+
+}
+
---
+++ src/automato.h
@@ -0,0 +1,34 @@
+// Alunos:
+// Denner Efisio Emanuel Reis - 201735008
+// Thomas Santos - 201776034
+// Igor Westermann Lima - 201876021
+
+#ifndef AUTOMATO_H_INCLUDED
+#define AUTOMATO_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include "automato.h"
+#include "tag.h"
+
+using namespace std;
+
+class Automato
+{
+private:
+    //string ;
+
+public:
+    void montaAutomato(Tag *tag);
+    void estadoFinal(Automato aut);
+    Automato automatoSimples(String simbolo, int i);
+    Automato automatoUniao(Automato aut1, Automato au2, int i);
+    Automato automatoConcatenacao(Automato aut1, aut2);
+    Automato criaFecho(Automato aut);
+
+};
+
+#endif // AUTOMATO_H_INCLUDED
